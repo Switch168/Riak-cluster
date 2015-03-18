f03311203df775b7c043aef096ff999f1a6fe616
Hussein Galal <hussein.galal.ahmed.11@gmail.com>
2015-03-17 20:10:03 -0400

    hello

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-IP_ADDRESS=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-
 if env | grep -q "RIAK_JOINING_IP"; then
   # Join node to the cluster
-  (sleep 5;riak-admin cluster join "riak@${RIAK_JOINING_IP}" > /dev/null 2>&1 & echo -e "Node ${IP_ADDRESS} Joined The Cluster") &
+  (sleep 5;riak-admin cluster join "riak@${RIAK_JOINING_IP}" > /dev/null 2>&1 && echo -e "Node Joined The Cluster") &
 
   # Are we the last node to join?
   (sleep 8; if riak-admin member-status | egrep "joining|valid" | wc -l | grep -q "${RIAK_CLUSTER_SIZE}"; then
-    riak-admin cluster plan > /dev/null 2>&1 && riak-admin cluster commit > /dev/null 2>&1 & echo -e "\nCommiting The Changes..."
+    riak-admin cluster plan > /dev/null 2>&1 && riak-admin cluster commit > /dev/null 2>&1 && echo -e "\nCommiting The Changes..."
   fi) &
 fi
 
